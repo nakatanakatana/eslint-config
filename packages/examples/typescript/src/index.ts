303a55355e72209c9e1bd3ce072b9765d2856e9d
tanaka <nakatanakatana@gmail.com>
2021-08-11 01:58:34 +0900

    add example

--- packages/examples/typescript/src/index.ts
+++ packages/examples/typescript/src/index.ts
@@ -1,9 +1,28 @@
 const hoge = '11234567890123456789012345678901234567890123456789012345678901234567890234567890'
-var fuga = "123";
-let foo = ''
+const fuga = "123";
 
 function main() {
-  if (foo) {}
+  for (let i=0; i++; i< 99) {
+    const foo = Math.floor(Math.random()*16)
+    switch (foo) {
+      case 1: console.log(1); break;
+      case 2: console.log(2); break;
+      case 3: console.log(3); break;
+      case 4: console.log(4); break;
+      case 5: console.log(5); break;
+      case 6: console.log(6); break;
+      case 7: console.log(7); break;
+      case 8: console.log(8); break;
+      case 9: console.log(9); break;
+      case 10: console.log(10); break;
+      case 11: console.log(11); break;
+      case 12: console.log(12); break;
+      case 13: console.log(13); break;
+      case 14: console.log(14); break;
+      case 15: console.log(15); break;
+      case 16: console.log(16); break;
+    }
+  }
 
   console.log(hoge, fuga);
 }
